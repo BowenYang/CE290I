dbe0797746852f2828ead69b80935801e6c0acdb
Bowen Yang <bo1992wen@berkeley.edu>
2014-09-03 16:30:39 -0700

    First push
    Tried something on the push staff.

---
+++ test 1.cpp
@@ -0,0 +1,16 @@
+//CE 290I Task 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int variable1, variable2;
+	for (int i = 0; i < variable1; ++i)
+	{
+		/* code */
+		variable2 ++;
+	}
+	
+	cout<<"Hello, World."<<endl;
+	return 0;
+}
